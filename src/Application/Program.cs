c583d710352db51fb0158cb432710138175d3a2f
Kalamojo <kojo22444488888888@gmail.com>
2025-09-04 15:07:25 -0400

    Added xml comment support

--- src/Application/Program.cs
+++ src/Application/Program.cs
@@ -1,13 +1,42 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var info = new OpenApiInfo
+    {
+        Title = "Your API Documentation",
+        Version = "v1",
+        Description = "Description of your API"
+    };
+
+    options.SwaggerDoc("v1", info);
+    
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "swagger/{documentName}/swagger.json";
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = "swagger";
+        options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Your API Title or Version");
+    });
+}
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
